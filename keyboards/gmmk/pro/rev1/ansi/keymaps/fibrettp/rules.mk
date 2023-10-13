e2edd1d716d94a519f56dfc1059e3ce22ddf5b03
FibreTTP <120446961+FibreTTP@users.noreply.github.com>
2023-10-13 19:00:16 +1100

    Copy rules from stickandgum and modify This explicitly states the debounce tyoe as tested by some guy on Reddit

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/fibrettp/rules.mk
@@ -0,0 +1,5 @@
+CONSOLE_ENABLE = no
+MOUSEKEY_ENABLE = yes
+
+# https://www.reddit.com/r/glorious/comments/qidnto/the_best_way_to_fix_gmmk_pro_input_latency_with
+DEBOUNCE_TYPE = asym_eager_defer_pk
